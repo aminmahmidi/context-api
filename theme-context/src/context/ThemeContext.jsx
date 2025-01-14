0776bea1d0099fb3de472b7d6cf711b6411b7501
Amin Mahmidi <mahmidismart@outlook.com>
2025-01-14 11:42:29 +0330

    fixed logic

--- theme-context/src/context/ThemeContext.jsx
+++ theme-context/src/context/ThemeContext.jsx
@@ -1,21 +1,38 @@
 import React, { createContext, useState, useEffect } from "react";
 
-const MyContext = createContext(null);
-const getTheme = () => {
-  const systemTheme = window.matchMedia("(prefers-color-scheme:dark)").matches;
-  const storedData = localStorage.getItem("dark") === "true";
-  return systemTheme || storedData;
-};
+const MyContext = createContext();
+// const getTheme = () => {
+//   const systemTheme = window.matchMedia("(prefers-color-scheme:dark)").matches;
+//   const storedData = localStorage.getItem("themeData") === "true";
+//   return storedData || systemTheme;
+// };
+
 const MyProvider = ({ children }) => {
-  const [theme, setTheme] = useState(getTheme());
-  const themeToggle = () => {
-    const newTheme = !theme;
-    setTheme(newTheme);
-    localStorage.setItem("theme-data", newTheme);
-  };
-  useEffect(() => {
-    document.body.classList.toggle("dark", theme);
-  }, [theme]);
+const [theme, setTheme] = useState(() => {
+  const savedTheme = localStorage.getItem("theme");
+  return (
+    savedTheme ||
+    (window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light")
+  );
+});
+useEffect(() => {
+  localStorage.setItem("theme", theme);
+  document.documentElement.setAttribute("data-theme", theme);
+}, [theme]);
+const themeToggle = () => {
+  setTheme(theme === "light" ? "dark" : "light");
+};
+  // const [theme, setTheme] = useState(getTheme());
+  // const themeToggle = () => {
+  //   const newTheme = !theme;
+  //   setTheme(newTheme);
+  // };
+  // useEffect(() => {
+  //   document.body.classList.toggle("dark", theme);
+  //   localStorage.setItem("themeData", theme);
+  // }, [theme]);
   return (
     <MyContext.Provider value={{ theme, setTheme, themeToggle }}>
       {children}
