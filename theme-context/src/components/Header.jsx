a461b410df4548266b846a5df970422c4c53adcc
Amin Mahmidi <mahmidismart@outlook.com>
2025-01-14 12:58:44 +0330

    added some classes

--- theme-context/src/components/Header.jsx
+++ theme-context/src/components/Header.jsx
@@ -2,13 +2,65 @@ import React, { useContext } from "react";
 import { MyContext } from "../context/ThemeContext";
 
 const Header = () => {
-  const { theme, setTheme, themeToggle } = useContext(MyContext);
+  const {
+    theme,
+    setTheme,
+    handleThemeChange,
+    setDropDown,
+    dropDown,
+    handleDropDown,
+  } = useContext(MyContext);
   return (
     <div className="header">
-      header
-      <button type="button" onClick={themeToggle}>
-        toggle
-      </button>
+      <div className="logo-container">
+        <h3 className="logo"> Amin Mahmidi</h3>
+      </div>
+
+      <div className="drop-down-container">
+        <button type="button" className="drop-down-btn" onClick={handleDropDown}>
+          تنظیمات تم
+        </button>
+        {
+          <div
+            className={
+              !dropDown
+                ? "show-drop-down drop-down"
+                : "hide-drop-down drop-down"
+            }
+          >
+            <label>
+              <input
+                type="radio"
+                name="theme"
+                value="light"
+                checked={theme === "light"}
+                onChange={handleThemeChange}
+              />
+              روشن
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="theme"
+                value="dark"
+                checked={theme === "dark"}
+                onChange={handleThemeChange}
+              />
+              تاریک
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="theme"
+                value="system"
+                checked={theme === "system"}
+                onChange={handleThemeChange}
+              />
+             سیستم
+            </label>
+          </div>
+        }
+      </div>
     </div>
   );
 };
--- theme-context/src/context/ThemeContext.jsx
+++ theme-context/src/context/ThemeContext.jsx
@@ -1,40 +1,56 @@
 import React, { createContext, useState, useEffect } from "react";
 
 const MyContext = createContext();
-// const getTheme = () => {
-//   const systemTheme = window.matchMedia("(prefers-color-scheme:dark)").matches;
-//   const storedData = localStorage.getItem("themeData") === "true";
-//   return storedData || systemTheme;
-// };
-
 const MyProvider = ({ children }) => {
-const [theme, setTheme] = useState(() => {
-  const savedTheme = localStorage.getItem("theme");
+  const [dropDown, setDropDown] = useState(false);
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme || "system";
+  });
+  useEffect(() => {
+    const prefersDarkScheme = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const selectedTheme =
+      theme === "system" ? (prefersDarkScheme ? "dark" : "light") : theme;
+    document.documentElement.setAttribute("data-theme", selectedTheme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+  const handleThemeChange = (event) => {
+    setTheme(event.target.value);
+  };
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleChange = () => {
+      if (theme === "system") {
+        document.documentElement.setAttribute(
+          "data-theme",
+          mediaQuery.matches ? "dark" : "light"
+        );
+      }
+    };
+    mediaQuery.addEventListener("change", handleChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, [theme]);
+
+  const handleDropDown = () => {
+    setDropDown(!dropDown);
+  };
+
   return (
-    savedTheme ||
-    (window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "light")
-  );
-});
-useEffect(() => {
-  localStorage.setItem("theme", theme);
-  document.documentElement.setAttribute("data-theme", theme);
-}, [theme]);
-const themeToggle = () => {
-  setTheme(theme === "light" ? "dark" : "light");
-};
-  // const [theme, setTheme] = useState(getTheme());
-  // const themeToggle = () => {
-  //   const newTheme = !theme;
-  //   setTheme(newTheme);
-  // };
-  // useEffect(() => {
-  //   document.body.classList.toggle("dark", theme);
-  //   localStorage.setItem("themeData", theme);
-  // }, [theme]);
-  return (
-    <MyContext.Provider value={{ theme, setTheme, themeToggle }}>
+    <MyContext.Provider
+      value={{
+        theme,
+        setTheme,
+        handleThemeChange,
+        setDropDown,
+        dropDown,
+        handleDropDown,
+      }}
+    >
       {children}
     </MyContext.Provider>
   );
